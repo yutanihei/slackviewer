4c0672694c5364a1345a6ab8d1700aefab537cc2
Yuta Nihei <becket.japan@gmail.com>
2019-07-29 19:53:21 +0900

    sadly another edit to wsgi

--- slackviewer/wsgi.py
+++ slackviewer/wsgi.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 
-from dj_static import Cling
+# from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "slackviewer.settings")
 
-application = Cling(get_wsgi_application())
+# application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
