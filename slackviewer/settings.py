dfe84662b14297da12dd2dcc4d55a876438d6b1d
Yuta Nihei <becket.japan@gmail.com>
2019-07-29 19:09:35 +0900

    edited DATABASES in sttings.py

--- slackviewer/settings.py
+++ slackviewer/settings.py
@@ -27,8 +27,7 @@ DEBUG = False
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
